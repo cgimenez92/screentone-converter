48c5d70ac00b9a2f7d2af361b6dd37641e421437
cgimenez92 <cristian.a.gimenez@gmail.com>
2025-05-15 10:30:10 -0300

    Cambio clase a funcion en image_loader

--- image_loader.py
+++ image_loader.py
@@ -1,15 +1,14 @@
 import cv2
 from tkinter import Tk, filedialog
 
-class ImageLoader:
-    def load():
-        Tk().withdraw()
-        filename = filedialog.askopenfilename(title="Select Image")
-        if not filename:
-            raise FileNotFoundError("No image selected.")
-        image_bgr = cv2.imread(filename)
-        if image_bgr is None:
-            return None, None
+def load_image():
+    Tk().withdraw()
+    filename = filedialog.askopenfilename(title="Select Image")
+    if not filename:
+        raise FileNotFoundError("No image selected.")
+    image_bgr = cv2.imread(filename)
+    if image_bgr is None:
+        return None, None
 
-        image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
-        return image_rgb, filename
+    image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+    return image_rgb, filename
